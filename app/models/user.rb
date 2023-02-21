d18ea733a2968d679eabf22f5b1bcb83991808b0
steakinzer <clementjodin1u@gmail.com>
2023-02-21 15:09:57 +0100

    deziojdozeijfoiezj

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,13 @@
 class User < ApplicationRecord
+  after_create :welcome_send
+
+  def welcome_send
+    UserMailer.welcome_email(self).deliver_now
+  end
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  def welcome_send
-    UserMailer.welcome_email(self).deliver_now
-  end
 
   has_many :events, through: :attendances
   has_many :attendances
--- config/environment.rb
+++ config/environment.rb
@@ -6,10 +6,10 @@ require_relative "application"
 Rails.application.initialize!
 
 ActionMailer::Base.smtp_settings = {
-  :user_name => ENV['SENDGRID_LOGIN'],
-  :password => ENV['SENDGRID_PWD'],
-  :domain => 'monsite.fr',
-  :address => 'smtp.sendgrid.net',
+  :user_name => ENV['JETMAIL_LOGIN'],
+  :password => ENV['JETMAIL_PWD'],
+  :domain => 'guarded-bastion-24087.herokuapp.com',
+  :address => 'in-v3.mailjet.com',
   :port => 587,
   :authentication => :plain,
   :enable_starttls_auto => true
