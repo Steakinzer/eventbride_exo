684499e9fe55613f91ff7ffd13aafe2881816202
steakinzer <clementjodin1u@gmail.com>
2023-02-21 16:04:56 +0100

    deziojdozeijfoiezjzdzdfzafdazdazd

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -39,35 +39,35 @@ Rails.application.configure do
 
   # Store uploaded files on the local file system (see config/storage.yml for options).
   config.active_storage.service = :local
-
   # Mount Action Cable outside main process or domain.
   # config.action_cable.mount_path = nil
   # config.action_cable.url = "wss://example.com/cable"
   # config.action_cable.allowed_request_origins = [ "http://example.com", /http:\/\/example.*/ ]
-
+  
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   # config.force_ssl = true
-
+  
   # Include generic and useful information about system operation, but avoid logging too much
   # information to avoid inadvertent exposure of personally identifiable information (PII).
   config.log_level = :info
-
+  
   # Prepend all log lines with the following tags.
   config.log_tags = [ :request_id ]
-
+  
   # Use a different cache store in production.
   # config.cache_store = :mem_cache_store
-
+  
   # Use a real queuing backend for Active Job (and separate queues per environment).
   # config.active_job.queue_adapter     = :resque
   # config.active_job.queue_name_prefix = "eventbride_production"
-
+  
   config.action_mailer.perform_caching = false
-
+  
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
-
+  config.action_mailer.default_url_options = { :host => "https://guarded-bastion-24087.herokuapp.com/" }
+  
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
   config.i18n.fallbacks = true
