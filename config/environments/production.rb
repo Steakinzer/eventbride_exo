7754424598e0f586b10af206c8896bfc591b4e1a
steakinzer <clementjodin1u@gmail.com>
2023-02-21 16:26:51 +0100

    dpeozkdpozekdpo

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -67,7 +67,6 @@ Rails.application.configure do
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
 
-  config.action_mailer.delivery_method = :letter_opener_web
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = false
 
